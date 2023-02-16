ba82f0264383211a69ab9bd4cffbe10128d9c8e5
MySQL Server <ivanxma@yahoo.com>
2023-02-16 03:22:02 +0000

    add script

---
+++ 02-configServers/01-startdb.sh
@@ -0,0 +1,3 @@
+ /usr/local/mysql/bin/mysqld_safe --defaults-file=config/my1.cnf &
+ /usr/local/mysql/bin/mysqld_safe --defaults-file=config/my2.cnf &
+ /usr/local/mysql/bin/mysqld_safe --defaults-file=config/my3.cnf &
---
+++ 02-configServers/02-checkdh.sh
@@ -0,0 +1,3 @@
+ /usr/local/mysql/bin/mysql -uroot -h127.0.0.1 -P3310 -e "select @@hostname, @@port;"
+ /usr/local/mysql/bin/mysql -uroot -h127.0.0.1 -P3320 -e "select @@hostname, @@port;"
+ /usr/local/mysql/bin/mysql -uroot -h127.0.0.1 -P3330 -e "select @@hostname, @@port;"
