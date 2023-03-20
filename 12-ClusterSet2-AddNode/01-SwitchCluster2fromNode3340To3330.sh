da77f966977135a0290aa85026495d36da026e08
Ivan Ma <ivanxma@yahoo.com>
2023-03-20 07:58:35 +0000

    update

--- 12-ClusterSet2-AddNode/01-SwitchCluster2fromNode3340To3330.sh
+++ 12-ClusterSet2-AddNode/01-SwitchCluster2fromNode3340To3330.sh
@@ -17,6 +17,29 @@ c1.addInstance('gradmin:grpass@`hostname`:3340', {exitStateAction:'OFFLINE_MODE'
 print(c1.status())
 "
 
+mysqlsh --uri gradmin:grpass@`hostname`:3310 -e "
+y = dba.getCluster('mycluster')
+y.removeInstance('`hostname`:3330')
+print(dba.getClusterSet().status({extended:1}))
+"
+
+mysqlsh --uri gradmin:grpass@`hostname`:3310 -e "
+y = dba.getClusterSet()
+y.createReplicaCluster('`hostname`:3330', 'mycluster2', {
+	consistency:'BEFORE_ON_PRIMARY_FAILOVER',
+	expelTimeout:30,
+	memberSslMode:'REQUIRED',
+	interactive:false,
+	autoRejoinTries:120,
+	memberWeight:80,
+	localAddress:'workshop23:3330',
+	recoveryMethod:'incremental'
+	})
+y = dba.getCluster('mycluster2')
+print(dba.getClusterSet().status({extended:1}))
+"
+
+
 
 
 
