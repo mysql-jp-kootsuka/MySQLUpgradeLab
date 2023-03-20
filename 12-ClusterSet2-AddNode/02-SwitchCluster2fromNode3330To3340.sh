8f16d486c3e73e51df0c5ea18a2033d214800ec4
Ivan Ma <ivanxma@yahoo.com>
2023-03-20 03:03:21 +0000

    Switching nodes with ClusterSet

--- 12-ClusterSet2-AddNode/02-SwitchCluster2fromNode3330To3340.sh
+++ 12-ClusterSet2-AddNode/02-SwitchCluster2fromNode3330To3340.sh
@@ -1,6 +1,23 @@
 . ./comm.sh
 
 mysqlsh --uri gradmin:grpass@`hostname`:3310 -e "
+y = dba.getClusterSet()
+y.removeCluster('mycluster2')
+print(y.status())
+"
+
+mysqlsh --uri gradmin:grpass@`hostname`:3310 -e "
+c1 = dba.getCluster('mycluster');
+c1.addInstance('gradmin:grpass@`hostname`:3330', {exitStateAction:'OFFLINE_MODE', 
+	recoveryMethod:'incremental', 
+	autoRejoinTries:120,
+	localAddress:'workshop23:3330',
+	memberWeight:70
+	})
+print(c1.status())
+"
+
+mysqlsh --uri gradmin:grpass@`hostname`:3310 -e "
 y = dba.getCluster('mycluster')
 y.removeInstance('`hostname`:3340')
 print(dba.getClusterSet().status({extended:1}))
