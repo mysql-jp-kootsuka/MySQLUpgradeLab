081da69ba6b2065e0e61f29a03a7b52ba650c251
Ivan Ma <ivanxma@yahoo.com>
2024-05-08 09:38:16 +0000

    us js for mysqlsh

--- 03-configGR/01-configAdmin.sh
+++ 03-configGR/01-configAdmin.sh
@@ -1,4 +1,4 @@
-mysqlsh -e "
+mysqlsh --js -e "
 dba.configureInstance('root:@localhost:3310',{clusterAdmin:'gradmin',clusterAdminPassword:'grpass'});
 dba.configureInstance('root:@localhost:3320',{clusterAdmin:'gradmin',clusterAdminPassword:'grpass'});
 dba.configureInstance('root:@localhost:3330',{clusterAdmin:'gradmin',clusterAdminPassword:'grpass'});
