1a2a3909144052ad9fe883dcf7aab09fa04a8eab
Kohei Otsuka <kohei.otsuka@oracle.com>
2024-08-21 05:22:31 +0000

    Fix * related error

--- templates/scripts/02_5.7to8.0/08_replicationCheck.sh
+++ templates/scripts/02_5.7to8.0/08_replicationCheck.sh
@@ -2,4 +2,4 @@
 
 ${MYSQL_DIR}/80sh/bin/mysqlsh root@localhost:3357 --sql -e\\"use world;INSERT INTO city VALUES (NULL, 'Tatebayashi', 'JPN', 'Gumma', 75440);\\"
 sleep 1
-${MYSQL_DIR}/80sh/bin/mysqlsh root@localhost:3380 --sql -e\\"use world;SELECT * FROM city WHERE Name='Tatebayashi';\\"
+${MYSQL_DIR}/80sh/bin/mysqlsh root@localhost:3380 --sql -e\\"use world;SELECT ID,Name,CountryCode,District,Population FROM city WHERE Name='Tatebayashi';\\"
--- templates/scripts/03_8.0to8.4/06_replicationCheckOn8.0.sh
+++ templates/scripts/03_8.0to8.4/06_replicationCheckOn8.0.sh
@@ -2,4 +2,4 @@
 
 ${MYSQL_DIR}/84sh/bin/mysqlsh root@localhost:3380 --sql -e\\"use world;INSERT INTO city VALUES (NULL, 'Tatsuno', 'JPN', 'Hyogo', 77970);\\"
 sleep 1
-${MYSQL_DIR}/84sh/bin/mysqlsh root@localhost:3384 --sql -e\\"use world;SELECT * FROM city WHERE Name='Tatsuno';\\"
+${MYSQL_DIR}/84sh/bin/mysqlsh root@localhost:3384 --sql -e\\"use world;SELECT ID,Name,CountryCode,District,Population FROM city WHERE Name='Tatsuno';\\"
