911f49c48798403c7c21dc0753dd303e78217c62
Kohei Otsuka <kochizufan@code4history.dev>
2024-08-21 12:03:34 +0900

    Escape )

--- templates/scripts/02_5.7to8.0/04_dataDump5.7.sh
+++ templates/scripts/02_5.7to8.0/04_dataDump5.7.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-${MYSQL_DIR}/80sh/bin/mysqlsh root@localhost:3357 --js -e\\"util.dumpInstance\\('${MYSQL_DIR}/sql/57dump')\\;\\"
+${MYSQL_DIR}/80sh/bin/mysqlsh root@localhost:3357 --js -e\\"util.dumpInstance\\('${MYSQL_DIR}/sql/57dump'\\)\\;\\"
