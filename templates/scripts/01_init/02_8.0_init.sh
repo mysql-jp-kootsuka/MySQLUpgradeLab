78f74c21dacf9712f294275ad6bf00b082103e5b
Kohei Otsuka <kochizufan@code4history.dev>
2024-08-21 15:21:13 +0900

    Add explanation to shell

--- templates/scripts/01_init/02_8.0_init.sh
+++ templates/scripts/01_init/02_8.0_init.sh
@@ -1,3 +1,31 @@
 #!/bin/bash
 
-${MYSQL_DIR}/80/bin/mysqld --defaults-file=${MYSQL_DIR}/configs/my80.cnf --initialize-insecure
+BIN=${MYSQL_DIR}/80/bin/mysqld
+CONF=${MYSQL_DIR}/configs/my80.cnf
+COMMAND1=\\"\\$BIN --defaults-file=\\$CONF --initialize-insecure\\"
+COMMAND2=\\"\\$BIN --defaults-file=\\$CONF &\\"
+
+echo \\"01-02: MySQL Server 8.0 の初期化を行います\\"
+echo \\"実行するスクリプト:\\"
+echo \\$COMMAND1
+echo 
+echo \\"MySQL 8.0 (\\$BIN) を、設定ファイル (\\$CONF) を用いて初期化します。\\"
+echo
+echo \\"設定ファイルの内容: \\"
+cat \\$CONF
+echo
+echo \\"ENTER キーを押すと実行します:\\"
+
+read tmp
+\\$COMMAND1 || echo \\"エラーが発生しました、環境を確認してください。\\" && exit 1
+
+echo \\"続いて MySQL Server 8.0 の起動を行います\\"
+echo \\"実行するスクリプト:\\"
+echo \\$COMMAND2
+
+echo \\"ENTER キーを押すと実行します:\\"
+
+read tmp
+\\$COMMAND2 || echo \\"エラーが発生しました、環境を確認してください。\\" && exit 1
+
+echo \\"MySQL Server 8.0 の起動が完了しました。\\"
--- templates/scripts/01_init/03_5.7_start.sh
+++ templates/scripts/01_init/03_5.7_start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-${MYSQL_DIR}/57/bin/mysqld --defaults-file=${MYSQL_DIR}/configs/my57.cnf &
+BIN=${MYSQL_DIR}/57/bin/mysqld
+CONF=${MYSQL_DIR}/configs/my57.cnf
+COMMAND=\\"\\$BIN --defaults-file=\\$CONF &\\"
+
+echo \\"01-01: MySQL Server 5.7 をスタートします\\"
+echo \\"実行するスクリプト:\\"
+echo \\$COMMAND
+echo 
+echo \\"ポート 3357 (設定ファイル内で指定) で待ち受ける MySQL Server 5.7 をスタートします\\"
+echo
+echo \\"ENTER キーを押すと実行します:\\"
+
+read tmp
+\\$COMMAND
