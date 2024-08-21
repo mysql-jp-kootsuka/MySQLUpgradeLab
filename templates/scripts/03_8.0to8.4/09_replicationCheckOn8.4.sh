f410f78c5ec2e03c0ea94c4f8c87b85f90d006f0
Kohei Otsuka <kochizufan@code4history.dev>
2024-08-21 12:23:06 +0900

    Escape *

--- templates/scripts/03_8.0to8.4/09_replicationCheckOn8.4.sh
+++ templates/scripts/03_8.0to8.4/09_replicationCheckOn8.4.sh
@@ -2,4 +2,4 @@
 
 ${MYSQL_DIR}/84sh/bin/mysqlsh root@localhost:3380 --sql -e\\"use world\\;INSERT INTO city VALUES \\(NULL, \\'Kashiba\\', \\'JPN\\', \\'Nara\\', 79020\\)\\;\\"
 sleep 1
-${MYSQL_DIR}/84sh/bin/mysqlsh root@localhost:3384 --sql -e\\"use world\\;SELECT * FROM city WHERE Name=\\'Kashiba\\'\\;\\"
+${MYSQL_DIR}/84sh/bin/mysqlsh root@localhost:3384 --sql -e\\"use world\\;SELECT \\* FROM city WHERE Name=\\'Kashiba\\'\\;\\"
